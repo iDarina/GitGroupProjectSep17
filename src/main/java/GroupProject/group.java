6e18d473cdb1ea5663430668e2b7e78caf987b50
iDaryna <dargud99@gmail.com>
2024-09-17 15:27:37 -0400

    shape class

--- src/main/java/GroupProject/group.java
+++ src/main/java/GroupProject/group.java
@@ -2,6 +2,6 @@ package GroupProject;
 
 public class group {
     public static void main(String[] args) {
-        
+
     }
 }
--- src/main/java/GroupProject/shape.java
+++ src/main/java/GroupProject/shape.java
@@ -1,4 +1,16 @@
 package GroupProject;
 
 public class shape {
+    public String color;
+    public double area;
+    public double perimeter;
+
+
+    public void calculateArea() {
+        System.out.println("This method calculates Area");
+    }
+
+    public void calculatePerimeter() {
+        System.out.println("This method calculates Perimeter");
+    }
 }
